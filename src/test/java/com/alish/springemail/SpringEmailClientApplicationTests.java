5e2a7550f61c34dd2d2122099af7851da5435763
alish <alish.dhamala@varosatech.co>
2022-08-27 21:57:57 +0545

    first commit

---
+++ src/test/java/com/alish/springemail/SpringEmailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alish.springemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
